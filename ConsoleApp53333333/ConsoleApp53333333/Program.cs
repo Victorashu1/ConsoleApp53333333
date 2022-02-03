dab65a9206eb108391cd3d337a5b077b634b6c12
Victorashu1 <98032021+Victorashu1@users.noreply.github.com>
2022-02-04 02:15:17 +0300

    Add files via upload

---
+++ ConsoleApp53333333/ConsoleApp53333333/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Text;
+using System.Collections.Specialized;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace http
+{
+    class Program
+    {
+        public static object MessageBox { get; private set; }
+
+        public void sendGET ()
+        {
+            // Адрес ресурса, к которому выполняется запрос
+            string url = "http://site.com/";
+
+            // Создаём объект WebClient
+            using (var webClient = new WebClient())
+            {
+                // Выполняем запрос по адресу и получаем ответ в виде строки
+                var response = webClient.DownloadString(url);
+            }
+        }
+        private static async Task PostRequestAsync(string mesage,string Types)
+        {
+       
+            WebRequest request = WebRequest.Create("http://127.0.0.1:8000/");
+            request.Method = Types; 
+            string data = ""+mesage;
+            byte[] byteArray = System.Text.Encoding.UTF8.GetBytes(data);
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = byteArray.Length;
+
+            using (Stream dataStream = request.GetRequestStream())
+            {
+                dataStream.Write(byteArray, 0, byteArray.Length);
+            }
+
+            WebResponse response = await request.GetResponseAsync();
+            using (Stream stream = response.GetResponseStream())
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    Console.WriteLine(reader.ReadToEnd());
+                }
+            }
+            response.Close();
+            Console.WriteLine("Запрос выполнен...");
+        }
+       
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Simple Http Server\n введите значение или show вывести все Get запросом");
+            var server = new SimpleHttpServer(Log);
+            server.Start();
+
+            while (true)
+            {
+                
+                var s = Console.ReadLine();
+                if(s=="show") break;
+                Task task1 = PostRequestAsync(s, "POST");
+               
+            }
+
+
+            string url = "http://127.0.0.1:8000/";
+
+            // Создаём объект WebClient
+            using (var webClient = new WebClient())
+            {
+                // Выполняем запрос по адресу и получаем ответ в виде строки
+                var response = webClient.DownloadString(url);
+                Log(response);
+            }
+
+       
+
+
+
+
+        }
+
+        private static void Log(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ ConsoleApp53333333/ConsoleApp53333333/SimpleHttpServer.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace http
+{
+    public class SimpleHttpServer
+    {
+        List<string> numsList = new List<string>();
+        
+        private HttpListener server;
+        private bool isRunning = false;
+        private Action<String> logger;
+        byte[] buffer;
+        public SimpleHttpServer(Action<String> logger)
+        {
+            this.logger = logger;
+            server = new HttpListener();
+            server.Prefixes.Add("http://127.0.0.1:8000/");
+            server.Prefixes.Add("http://localhost:8000/");
+        }
+
+        public void Stop()
+        {
+            isRunning = false;
+            server.Stop();
+        }
+        public void GetList() {
+            foreach (var item in numsList)
+            {
+                Console.Write(item + "\\n");
+            }
+            Console.Write("end");
+
+        }
+
+
+        public void setPOST(HttpListenerContext context)
+        {
+
+
+
+            var request = context.Request;
+
+
+            var body = "";
+            if (request.HasEntityBody)
+            {
+                body = readFromStream(request.InputStream);
+                logger($">>> {body}");
+            }
+
+            HttpListenerResponse response = context.Response;
+
+            buffer = Encoding.UTF8.GetBytes(body);
+            Console.WriteLine(response.ToString());
+
+            numsList.Add(body);
+     
+            response.ContentLength64 = buffer.Length;
+
+            Stream st = response.OutputStream;
+
+            st.Write(buffer, 0, buffer.Length);
+
+            response.Close();
+            Console.WriteLine("элемент добвлен в список");
+        }
+
+
+
+
+
+        public void Start()
+        {
+            isRunning = true;
+            server.Start();
+            new Task(() => {
+                while (isRunning)
+                {
+                    try
+                    {
+                        handleConnection(server.GetContext());
+                    }
+                    catch (Exception e)
+                    {
+                        logger($"HttpServer connection handle error: {e}");
+                    }
+                }
+            }).Start();
+        }
+
+        private void handleConnection(HttpListenerContext context)
+        {
+            var request = context.Request;
+            //logger(">>> Connected");
+            logger($">>> {request.HttpMethod} {request.Url?.AbsoluteUri}");
+           
+
+            if (request.HttpMethod == "GET")
+            {
+
+                GetList();
+                return;
+            }
+
+
+
+            if (request.HttpMethod == "POST")
+            {
+
+                setPOST( context);
+                return;
+            }
+
+           
+
+
+
+               
+
+
+
+           // logger(">>> end handle connection");
+        }
+
+
+
+
+
+        private String readFromStream(Stream stream)
+        {
+            var input = new StreamReader(stream, Encoding.UTF8);
+            return input.ReadToEnd();
+        }
+    }
+}
